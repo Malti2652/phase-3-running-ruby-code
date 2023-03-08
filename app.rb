4e2cd3809981ca9c3be963738560f5b4f68c5419
Eddington Rowellis <eddingtonibi@gmail.com>
2023-03-08 10:44:58 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -12,12 +12,8 @@
     
 # end
 # # 1 + 1
-# puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
+# app.rb
+puts "Hello World!"
+print "Pass this test, please." 
+p [1, 2, 3]
